44b8eae8f1f91f6daf14a77e03c24c3a0d4ad90b
Yugendran S <119640986+111622104117@users.noreply.github.com>
2025-07-09 03:20:12 +0530

    Update urls.py

--- CodeAlpha_SocialMediaApp/config/urls.py
+++ CodeAlpha_SocialMediaApp/config/urls.py
@@ -8,3 +8,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('social.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
